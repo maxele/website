e136b85afe04f3e16bf3e1093ebb93573fbe7fb2
maxeleee <maxmaxeleee@gmail.com>
2022-11-30 11:20:48 +0100

    Added a button to toggle sidebar

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// document.getElementById('sidebar-button').value = "C\nL\nO\nS\nE"
+document.getElementById('sidebar-button').value = "M\nE\nN\nU"
+
 sections = 20
 function randomvalue(i, prev) {
   ret = prev + Math.random() * 10 - 5 - (100 / sections)
@@ -53,11 +56,15 @@ var sizeCheck = function () {
     document.getElementById('sidebar').style.transform = "translate(-100%)"
     document.getElementById('text').style = "width: 90%"
     document.getElementById('mobile-menu-button').style.display = ""
+
+    document.getElementById('sidebar-button').style.transform = "translateY(-50%) translateX(-100%)"
   } else {
     exitMobileMenu();
     document.getElementById('sidebar').style.transform = "translate(0%)"
     document.getElementById('text').style = "width: 50%"
     document.getElementById('mobile-menu-button').style.display = 'none'
+
+    document.getElementById('sidebar-button').style.transform = "translateY(-50%) translateX(-50%)"
   }
 }
 addEventListener("resize", sizeCheck);
@@ -74,9 +81,28 @@ var exitMobileMenu = function () {
   document.getElementById('mobile-menu-button').value = "MENU"
   document.getElementById('mobile-menu-button').style = "transform: translate(-50%);\
     bottom: 10px;"
-  document.getElementById('mobile-menu').style = "\
-    bottom: -65%;"
+  document.getElementById('mobile-menu').style = "bottom: -65%;"
   document.getElementById('mobile-menu-button').onclick = mobileMenu
+}
+
+var sidebar = function() {
+  // document.getElementById('sidebar-button').value = "C\nL\nO\nS\nE"
+  document.getElementById('sidebar-button').style.transform = "translateY(-50%) translateX(-50%)"
+  document.getElementById('sidebar-button').onclick = exitSidebar
+
+  document.getElementById('sidebar').style.transform = "translateX(0%)"
+
+  document.getElementById('text').style.width = '50%'
+}
+
+var exitSidebar = function() {
+  // document.getElementById('sidebar-button').value = "M\nE\nN\nU"
+  document.getElementById('sidebar-button').style.transform = "translateY(-50%) translateX(50%)"
+  document.getElementById('sidebar-button').onclick = sidebar
+
+  document.getElementById('sidebar').style.transform = "translateX(-100%)"
+
+  document.getElementById('text').style.width = '80%'
 }
 
 myScrollFunc()
